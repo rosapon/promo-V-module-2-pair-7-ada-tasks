e10bdca50f727f161b07492945297bdb22a4800d
94Jenn <151082727+94Jenn@users.noreply.github.com>
2024-01-16 11:20:35 +0100

    Servidor Peticions

--- main.js
+++ main.js
@@ -1,104 +1,70 @@
-'use strict' 
+let tasks = [];
 
-const inputNewTask = document.querySelector('.js-text-task-add');
-const addBtn = document.querySelector('.js-btn-add');
-const inputFilter = document.querySelector('.js-text-task-filter');
-const searchBtn = document.querySelector('js-btn-filter');
-const taskList = document.querySelector('.js-task-list');
-const inputList = document.querySelector('.js-inputList');
-const li = document.querySelector('.js-li');
+const taskList = document.querySelector(".task-list");
+const btnFilter = document.querySelector(".js-btn-filter");
+const inputFilter = document.querySelector(".js-text-task-filter");
 
+function handleFilter(event) {
+  event.preventDefault();
+  const valueInput = inputFilter.value;
+  const arrayFilter = tasks.filter((task) => task.name.includes(valueInput));
+  console.log(arrayFilter);
+  renderTasks(arrayFilter);
+}
+btnFilter.addEventListener("click", handleFilter);
 
-
-
-const tasks = [
-    { name: 'Recoger setas en el campo', completed: true },
-    { name: 'Comprar pilas', completed: true },
-    { name: 'Poner una lavadora de blancos', completed: true },
-    {
-      name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-      completed: false,
-    },
-  ];
-
-console.log(tasks[0]);
-
-
-
-const renderTask = () => {
-    for (let eachTask of tasks) {
-    taskList.innerHTML += `<li>
-    <input type="checkbox">
-    <label class="item">${eachTask.name}</label>
-</li>`
-    };
-    
-    if (eachTask === chekbox.checked) {
-        eachTask.classList.add('tachado')
+const listenCheck = () => {
+  const allCheckbox = document.querySelectorAll(".js-check");
+  for (const check of allCheckbox) {
+    check.addEventListener("change", handleCheck);
+  }
+};
+const renderTasks = (tasks) => {
+  taskList.innerHTML = "";
+  for (let index = 0; index < tasks.length; index++) {
+    /*let classCss = tasks[index].completed ? 'tachado' : null;
+    taskList.innerHTML += `<li class= "${classCss}"> 
+      ${tasks[index].name}
+    </li>`;*/
+    if (tasks[index].completed) {
+      //tasks[index].completed === true
+      taskList.innerHTML += `<li class= "tachado"> 
+      <input type="checkbox" class= "js-check" id="${index}" checked>
+      ${tasks[index].name}
+    </li>`;
     } else {
-        eachTask.classList.remove('tachado')
-    };
+      taskList.innerHTML += `<li> 
+      <input type="checkbox" class= "js-check" id="${index}" >
+      ${tasks[index].name}
+    </li>`;
+    }
+  }
+  listenCheck();
 };
 
+function handleCheck(event) {
+  const id = event.target.id;
+  console.log(id);
+  tasks[id].completed = !tasks[id].completed;
+  console.log(tasks);
+  renderTasks(tasks);
+}
+renderTasks(tasks);
+/*
+  pintar elementos en html
+  escuchar eventos 
+  una vez escuchas cambias los datos basado en el evento
+  volver a pintar, volver a escuchar
+  */
 
 
-
-console.log(renderTask);
-renderTask();
-
-
-
-
-
-/* const handleClick = (event) => {
-    event.preventDefault();
-    let userInput = inputNewTask.value;
-    taskList.innerHTML += `<li>
-    <input type="checkbox">
-    <label class="item">${userInput}</label>
-</li>`
-   /*  tasks.push(userInput); */
-
-
-
-
-// condicional para saber si la tarea es completed: true
-
-/* function checkedTask(event) {
-    const checkbox = event.currentTarget;
-    const listItem = checkbox.parentNode('li');
-    
-    if (listItem) {
-        if (checkbox.checked) {
-            listItem.classList.add('tachado');
-        } else {
-            listItem.classList.remove('tachado');
-        }
-    }
-} */
-
-
-  
-/* taskList.addEventListener('input', checkedTask); */
-/* addBtn.addEventListener('click', handleClick); */
-
-
-/* const handleSearch = tasks.filter((task) => {
-    if (task === inputFilter.value ) {
-        taskList.innerHTML += `<li>
-    <input type="checkbox">
-    <label class="item">${inputFilter.value}</label>
-</li>`
-    } else {
-        taskList.innerHTML = "";
-    }
-
+//2.13. Peticiones al servidor. 
+fetch(`https://dev.adalab.es/api/todo`)
+.then((response)=> response.json())
+.then((data)=> {
+    tasks = data.results;
+    console.log(tasks);
+    renderTasks(tasks); //pinta el listado en el html (punto 5 del 2.13)
 })
-    
- */
-
-
-
-/* searchBtn.addEventListener('click', handleSearch) */
 
 
